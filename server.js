807335e542ce8bd263136e62eb7811689db9ee09
ahmedcalif88 <ahmedcalif88@gmail.com>
2024-04-29 14:33:51 -0700

    added new folders

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const bodyParser = require('body-parser');
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
     }
 );
+
+
 
 app.listen(port, () => {
 console.log('Server is running on port 8000');
