2088c09986c729711a343b72567506bd7261d7d8
ahmedcalif88 <ahmedcalif88@gmail.com>
2024-07-17 13:23:47 -0700

    feat: added a error message page when there are no liked posts

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -1,141 +1,169 @@
-const router = require('express').Router();
-const { User, Avatar, Post, Like } = require('../database/schema/schemaModel');
-const { ensureAuthenticated } = require('../middleware/middleware');
-const multer = require('multer');
+const router = require("express").Router();
+const { User, Avatar, Post, Like } = require("../database/schema/schemaModel");
+const { ensureAuthenticated } = require("../middleware/middleware");
+const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../cloudinaryConfig.js");
-const path = require('path');
+const path = require("path");
 
 function uploadMiddleware(folderName) {
   const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: (req, file) => {
-        const folderPath = `${folderName.trim()}`;
-        const fileExtension = path.extname(file.originalname).substring(1);
-        const publicId = `${req.session.userId}-${file.fieldname}`;
-        
-        return {
-            folder: folderPath,
-            public_id: publicId,
-            format: fileExtension,
-            overwrite: true,
-        };
-        
+      const folderPath = `${folderName.trim()}`;
+      const fileExtension = path.extname(file.originalname).substring(1);
+      const publicId = `${req.session.userId}-${file.fieldname}`;
+
+      return {
+        folder: folderPath,
+        public_id: publicId,
+        format: fileExtension,
+        overwrite: true,
+      };
     },
-});
-return multer({ storage: storage });
+  });
+  return multer({ storage: storage });
 }
-
-
 
 const uploadFile = uploadMiddleware("/uploads");
 
-router.get('/', ensureAuthenticated, async (req, res) => {
-    try {
-        const user = await User.findOne({
-            where: { username: req.session.username },
-            include: [{ model: Avatar }]
-        });
-        const likedPosts = await Post.findAll({
-            include: [{
-                model: Like,
-                as: 'Likes',
-                required: true,
-                where: { userId: user.userId }  
-            }, {
-                model: User,
-                as: 'creator',
-                attributes: ['username'],
-                include: [{
-                    model: Avatar,
-                    attributes: ['imageUrl']
-                }]
-            }]
-        });
+router.get("/", ensureAuthenticated, async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.session.username },
+      include: [{ model: Avatar }],
+    });
+    const likedPosts = await Post.findAll({
+      include: [
+        {
+          model: Like,
+          as: "Likes",
+          required: true,
+          where: { userId: user.userId },
+        },
+        {
+          model: User,
+          as: "creator",
+          attributes: ["username"],
+          include: [
+            {
+              model: Avatar,
+              attributes: ["imageUrl"],
+            },
+          ],
+        },
+      ],
+    });
 
-        const avatarUrl = user.Avatar ? user.Avatar.imageUrl : 'https://i.pravatar.cc/150?img=3';
-        res.render('profile/profile', { user: user, avatar: avatarUrl, likedPosts: likedPosts });
-    } catch (error) {
-        console.error('Error fetching user data:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    const avatarUrl = user.Avatar
+      ? user.Avatar.imageUrl
+      : "https://i.pravatar.cc/150?img=3";
+    res.render("profile/profile", {
+      user: user,
+      avatar: avatarUrl,
+      likedPosts: likedPosts,
+    });
+  } catch (error) {
+    console.error("Error fetching user data:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-router.post('/upload-avatar', ensureAuthenticated, uploadFile.single('avatar'), async (req, res) => {
+router.post(
+  "/upload-avatar",
+  ensureAuthenticated,
+  uploadFile.single("avatar"),
+  async (req, res) => {
     if (!req.file) {
-        console.log("No file uploaded.");
-        return res.status(400).json({ error: 'No file uploaded.' });
+      console.log("No file uploaded.");
+      return res.status(400).json({ error: "No file uploaded." });
     }
     try {
-        const userId = req.session.userId;
-        const user = await User.findByPk(userId);
-        if (!user) {
-            console.log("User not found.");
-            return res.status(404).json({ error: 'User not found.' });
-        }
-        const avatarUrl = req.file.path; 
-        const existingAvatar = await Avatar.findOne({ where: { userId: userId } });
-        if (existingAvatar) {
-            await Avatar.update({
-                imageUrl: avatarUrl,
-                uploadDate: new Date()
-            }, {
-                where: { userId: userId }
-            });
-        } else {
-            await Avatar.create({
-                userId: userId,
-                imageUrl: avatarUrl,
-                uploadDate: new Date()
-            });
-        }
-       res.redirect('back')
-    } catch (error) {
-        console.error('Failed to upload avatar:', error.stack);
-        return res.status(500).json({ error: 'Failed to upload avatar.' });
-    }
-});
-
-router.get('/liked-posts', ensureAuthenticated, async (req, res) => {
-    try {
-        const user = await User.findOne({
-            where: { username: req.session.username },
-            include: [{ model: Avatar }]
+      const userId = req.session.userId;
+      const user = await User.findByPk(userId);
+      if (!user) {
+        console.log("User not found.");
+        return res.status(404).json({ error: "User not found." });
+      }
+      const avatarUrl = req.file.path;
+      const existingAvatar = await Avatar.findOne({
+        where: { userId: userId },
+      });
+      if (existingAvatar) {
+        await Avatar.update(
+          {
+            imageUrl: avatarUrl,
+            uploadDate: new Date(),
+          },
+          {
+            where: { userId: userId },
+          }
+        );
+      } else {
+        await Avatar.create({
+          userId: userId,
+          imageUrl: avatarUrl,
+          uploadDate: new Date(),
         });
-
-        if (!user) {
-            return res.status(404).send("User not found");
-        }
-
-        const avatarUrl = user.Avatar ? user.Avatar.imageUrl : 'https://i.pravatar.cc/150?img=3';
-
-        const likedPosts = await Post.findAll({
-            include: [{
-                model: Like,
-                as: 'Likes',
-                required: true,
-                where: { userId: user.userId }  
-            }, {
-                model: User,
-                as: 'creator',
-                attributes: ['username'],
-                include: [{
-                    model: Avatar,
-                    attributes: ['imageUrl']
-                }]
-            }]
-        });
-
-        if (likedPosts.length === 0) {
-            return res.status(404).send("No liked posts found");
-        }
-
-        res.render('profile/likedPosts', { user: user, likedPosts: likedPosts, avatar: avatarUrl });
+      }
+      res.redirect("back");
     } catch (error) {
-        console.error("Failed to retrieve liked posts:", error);
-        res.status(500).send("Error retrieving liked posts");
+      console.error("Failed to upload avatar:", error.stack);
+      return res.status(500).json({ error: "Failed to upload avatar." });
     }
-});
+  }
+);
 
+router.get("/liked-posts", ensureAuthenticated, async (req, res) => {
+  try {
+    const user = await User.findOne({
+      where: { username: req.session.username },
+      include: [{ model: Avatar }],
+    });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const avatarUrl = user.Avatar
+      ? user.Avatar.imageUrl
+      : "https://i.pravatar.cc/150?img=3";
+
+    const likedPosts = await Post.findAll({
+      include: [
+        {
+          model: Like,
+          as: "Likes",
+          required: true,
+          where: { userId: user.userId },
+        },
+        {
+          model: User,
+          as: "creator",
+          attributes: ["username"],
+          include: [
+            {
+              model: Avatar,
+              attributes: ["imageUrl"],
+            },
+          ],
+        },
+      ],
+    });
+
+    if (likedPosts.length === 0) {
+      res.render("profile/errorMessage");
+    } else {
+      res.render("profile/likedPosts", {
+        user: user,
+        likedPosts: likedPosts,
+        avatar: avatarUrl,
+      });
+    }
+  } catch (error) {
+    console.error("Failed to retrieve liked posts:", error);
+    res.status(500).send("Error retrieving liked posts");
+  }
+});
 
 module.exports = router;
